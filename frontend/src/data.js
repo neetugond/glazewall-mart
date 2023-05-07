36d0211e39afca1539b77b2d57ec2e4e018ca1bb
neetugond <ngond92@gmail.com>
2023-05-08 03:38:51 +0530

    data added from render method

---
+++ frontend/src/data.js
@@ -0,0 +1,77 @@
+export default {
+    products: [
+        {
+            _id: '1',
+            name: 'anchor fashener',
+            category: 'Hardware',
+            image: 'https://5.imimg.com/data5/SELLER/Default/2022/2/CN/XP/GQ/122558109/mungo-fasteners-500x500.jpg',
+            price: '60',
+            brand: 'mungo',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock:6,
+            
+        },
+        {
+            _id: '2',
+            name: 'Hydraulic Patch Fittings',
+            category: 'Hardware',
+            image: 'https://www.ozonehardware.com/assets/img/Hydraulic-Patch-Fittings.png',
+            price: '150',
+            brand: 'ozone',
+            rating: 5,
+            numReviews: 10,
+            countInStock:12,
+            
+        },
+        {
+            _id: '3',
+            name: 'AC Brushless Motor',
+            category: 'powertool',
+            image: 'https://www.hikoki-powertools.com/products/powertools/ac-grind/g10byeq2/g10byeq2.jpg',
+            price: '500',
+            brand: 'Hikoki',
+            rating: 2.5,
+            numReviews: 5,
+            countInStock:7,
+            
+        },
+        {
+            _id: '4',
+            name: 'OEM Manufacturing',
+            category: 'Hardware',
+            image: 'https://www.ozonehardware.com/assets/img/OEM-Manufacturing.png',
+            price: '120',
+            brand: 'ozone',
+            rating: 4.5,
+            numReviews: 15,
+            countInStock:10,
+            
+        },
+        {
+            _id: '5',
+            name: 'Sensors Doors & Intelligent Locks ',
+            category: 'hardware',
+            image: 'https://www.ozonehardware.com/assets/img/Sensors-Doors-Intelligent-Locks.png',
+            price: '1300',
+            brand: 'ozone',
+            rating: 3.5,
+            numReviews: 8,
+            countInStock:9,
+            
+        },
+        {
+            _id: '6',
+            name: 'wall anchor fashener',
+            category: 'Hardware',
+            image: 'https://www.aplhome.com/img/productimages/Hexheadscrew_0933.png',
+            price: '150',
+            brand: 'mungo',
+            rating: 3,
+            numReviews: 5,
+            countInStock:10,
+            
+        },
+       
+    ]
+}
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,33 @@
+import data from '../data.js'
+const HomeScreen = {
+    render: () => {
+        const { products } = data;
+        return `
+        <ul class="products">
+        ${products.map((product) => `
+        <li>
+        <div class="product">
+            <a href="/#/prpduct/${product._id}">
+                <img src="${product.image}" alt="product 1">
+            </a>
+            <div class="product-name">
+                <a href="/#/product/1">
+                    ${product.name}
+                </a>
+            
+            </div>
+            <div class="product-brand">
+                ${product.brand}
+            </div>
+            <div class="product-price">
+                ${product.price}
+            </div>
+        </div>
+    </li>
+        `).join('\n')}
+    </ul>`;
+    },
+};
+
+export default HomeScreen;
+
